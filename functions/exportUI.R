161478cfda0462fc3b0cfdc747d52999e4de1394
Tim Martin <Tim.Martin@state.mn.us>
2020-12-22 11:19:52 -0600

    Initial commit

---
+++ functions/exportUI.R
@@ -0,0 +1,88 @@
+output$exportUI = renderUI({
+  fluidRow(
+    column(
+      width = 9,
+      box(
+        title = "Export Data",
+        solidHeader = TRUE,
+        width = NULL,
+        status = "primary",
+        fluidRow(
+          column(
+            width = 6,
+            tags$br(),
+            actionBttn(
+              inputId = "exportprocessbttn",
+              label = "Process Data for Export",
+              style = "fill",
+              color = "success",
+              size = "lg"
+            )
+          ),
+          column(
+            width = 6,
+            uiOutput("exportviewUI")
+          )
+        ),
+        tags$br(),
+          # tags$h3("Data Preview"),
+        box(
+          title = "Data Preview",
+          collapsible = TRUE,
+          collapsed = TRUE,
+          width = NULL,
+          status = "success",
+          DTOutput("finaltable")
+        ),
+        tags$br(),
+        downloadBttn(
+          outputId = "dlddata",
+          label = "Download Data",
+          style = "material-flat",
+          # color = "success",
+          size = "lg"
+        )
+      )
+    ),
+    column(
+      width = 3
+    )
+  )
+})
+
+#Options for selecting the logger type if data are processed into separate files
+output$exportviewUI = renderUI({
+  if (!is.null(finaldata())){
+    if (is.data.frame(finaldata())){
+    }else{
+      pickerInput(
+        inputId = "exportviewloggertype",
+        label = "Logger Types",
+        #qcloggertypes() sourced from processing.R
+        choices = qcloggertypes()
+      )
+    }
+  }else{}
+})
+
+#Data Preview Table
+output$finaltable = renderDT(
+  options = list(
+    lengthChange = FALSE,
+    searching = FALSE
+  ),
+  rownames = FALSE,
+  extensions = 'Responsive',
+  {
+    if (!is.null(finaldata())){
+      finaldatatable = finaldata()
+      print(is.data.frame(finaldatatable))
+      if (is.data.frame(finaldatatable)){
+        return(finaldatatable)
+      }else{
+        finaldatatableselect = finaldatatable[[input$exportviewloggertype]]
+        return(finaldatatableselect)
+      }
+    }else{}
+  })
+
