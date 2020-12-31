f157c444a1cd7571473d2384a0c93c345ac8f777
Tim Martin <Tim.Martin@state.mn.us>
2020-12-31 15:30:00 -0600

    updated description

--- functions/saveconfigfile.R
+++ functions/saveconfigfile.R
@@ -27,8 +27,8 @@ output$saveconfigbttn = downloadHandler(
 output$saveconfigdescUI = renderUI({
   tags$p(
     HTML("<font size = 4><i>"),
-    "The configuration file contains all of the program, waterbodies, qc, and logger model settings, as well as logger processing history and metadata. Save this file after making 
-    any changes to these settings.",
+    "The configuration file contains all of the program, waterbodies, qc, and logger model settings, as well as logger processing history 
+    and metadata. Save this file after making any changes to these settings.",
     HTML("</i></font>")
     
   )
