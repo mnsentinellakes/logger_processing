400941c2c9dff5301efd6d282e08567fe0a7ddbe
Tim Martin <Tim.Martin@state.mn.us>
2022-10-14 15:09:23 -0500

    Fixed error when IncZ == FALSE

--- functions/alerts.R
+++ functions/alerts.R
@@ -3,13 +3,17 @@
 #Observes the add button. If the Depth text input is blank, an alert will popup, telling the user to fill out the input
 observeEvent(
   input$addbttn,{
-    if (nchar(input$inputdepth)==0){
-      sendSweetAlert(
-        session,
-        title = "Missing Z Value",
-        text = "Ignore if Z Value has been purposely omitted",
-        type = "warning"
-      )
+    includez = fieldnames()
+    includez = includez$IncZ
+    if (includez == TRUE){
+      if (nchar(input$inputdepth)==0){
+        sendSweetAlert(
+          session,
+          title = "Missing Z Value",
+          text = "Ignore if Z Value has been purposely omitted",
+          type = "warning"
+        )
+      }
     }
   }
 )
